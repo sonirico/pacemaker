cab6224de58e5a315f29cc8491d2e0b6f47b5564
Marcos <marcos@atani.com>
2023-05-24 18:32:27 +0200

    chore: add more integration tests

--- it/testing.go
+++ it/testing.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func assertFreeSlots(t *testing.T, expected, actual int64) {
 	t.Helper()
@@ -13,5 +16,17 @@ func assertNoError(t *testing.T, err error) {
 	t.Helper()
 	if err != nil {
 		t.Errorf("unexpected error, want none, have %v", err)
+	}
+}
+
+func assertError(t *testing.T, expected, actual error) {
+	t.Helper()
+
+	if actual == nil {
+		t.Errorf("expected error, have none")
+	}
+
+	if !errors.Is(expected, actual) {
+		t.Errorf("expected type of %v, have %v", expected, actual)
 	}
 }
